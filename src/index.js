7b81dee3ee812760ed20dc51d6090479dfbc888a
Oscar Luntha <lunthaoscar@gmail.com>
2026-01-20 16:02:16 +0200

    add event listeners to buttons for tab navigation

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@ import { loadHomePage } from "./pageload"
 import { loadContact } from "./contact"
 import { loadMenu } from "./menu"
 const contentDiv = document.querySelector("#content")
-contentDiv.appendChild(loadMenu())
+const tabButtons = document.querySelectorAll("button")
+contentDiv.appendChild(loadHomePage())
+tabButtons.forEach(button => {
+    button.addEventListener("click", ()=> {
+        if(button.id == "home"){
+            contentDiv.replaceChildren(loadHomePage())
+        }else if (button.id == "menu"){
+            contentDiv.replaceChildren(loadMenu())
+        }else{
+            contentDiv.replaceChildren(loadContact())
+        }
+    })
+})
+
 
